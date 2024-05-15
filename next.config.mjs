475d8effd9cb57cb1afbcc1c699f0f6c85b659cb
Shawn Pradeep <shawnprad@hotmail.com>
2024-05-14 23:02:31 -0400

    updated path for assets

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  basePath: "/tifftorrey",
+  output: "export",
+  reactStrictMode: true,
+  images: {
+    unoptimized: true,
+  },
+};
 
 export default nextConfig;
